4bec3534040d078400676f4a8e04985f132bf96c
cakin24 <798103175@qq.com>
2018-12-01 14:03:37 +0800

    2018-12-1

---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+
+def index(request):
+    article = models.Article.objects.get(pk=1)
+    return render(request, 'blog/index.html',{'article': article})
